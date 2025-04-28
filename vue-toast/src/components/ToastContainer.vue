7835b7ac3ef3b8309d721ffd2e58745f22791e39
natamox <natamoxc@gmail.com>
2025-04-28 13:54:33 +0800

    fix

--- vue-toast/src/components/ToastContainer.vue
+++ vue-toast/src/components/ToastContainer.vue
@@ -1,7 +1,13 @@
 <template>
-
+  <div>
+    <div v-for="toast in toasts" :key="toast.id">
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useToastContext } from '../composables/useToast'
+
+const { toasts } = useToastContext()
 
 </script>
